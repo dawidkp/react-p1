aa063efc99577e66c4fece7432ff84c1965f9554
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2023-03-16 14:02:37 +0100

    add current date

--- src/features/WeatherForecast/WeatherForecast.js
+++ src/features/WeatherForecast/WeatherForecast.js
@@ -1,10 +1,19 @@
+import React from 'react';
+
 import './WeatherForecast.scss';
 
 function WeatherForecast() {
+  const now = new Date();
+  const currentDate = `${now.getDate()}.0${now.getMonth()+1}.${now.getFullYear()}`  
+
   return (
-    <div className="weather-forecast">
-        ___WeatherForecast___
-    </div>
+    <section className="weather__forecast">
+      <div className="weather__box">
+        <h1>5 days forecast</h1>
+        <h1>{currentDate}</h1>
+
+      </div>
+    </section>
   );
 }
 
