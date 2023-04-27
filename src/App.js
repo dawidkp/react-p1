f99f0df5a443afcc73e79186e7479077b502bfba
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2023-04-27 14:05:32 +0200

    add Input Component

--- src/App.js
+++ src/App.js
@@ -5,16 +5,6 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   
-  // var requestOptions = {
-  //   method: 'GET',
-  //   redirect: 'follow'
-  // };
-  
-  // fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min,windspeed_10m_max&timezone=auto", requestOptions)
-  //   .then(response => response.text())
-  //   .then(result => console.log(result))
-  //   .catch(error => console.log('error', error));
-
   return (
     <div className="app">
       {/*Header*/}
