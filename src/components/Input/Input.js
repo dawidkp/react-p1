a714b6b36c5597eafb813665404f9bdc8ece1f53
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2023-04-27 14:14:06 +0200

    add options

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Input = () =>{
-    
+const Input = ({
+    type = 'text' | 'email' | 'password' | 'date' | 'number',
+    title,
+    placeholder,
+    label,
+    autofocus,
+    disabled,
+    errorMessage,
+    onChange,
+    value
+
+}) => {
+   
 }
 
 export default Input;
